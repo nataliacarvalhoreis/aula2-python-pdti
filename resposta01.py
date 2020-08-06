7c4bcd8c25317a9928886b95c7f804f43e67ee3a
nataliacarvalhoreis <nataliacarvalhoreis@gmail.com>
2020-08-05 21:41:33 -0300

    primeiro commit

---
+++ resposta01.py
@@ -0,0 +1,7 @@
+num1 = int(input("Informe o primeiro numero:"))
+num2 = int(input("Informe o segundo numero:"))
+
+if(num1>num2):
+    print("O maior número é: ", num1)
+else:
+    print("O maior número é: ", num2)

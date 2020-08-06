f037e9705223608160040f6d0337cc6a9442bcf8
nataliacarvalhoreis <nataliacarvalhoreis@gmail.com>
2020-08-05 21:57:27 -0300

    primeiro commit

---
+++ resposta04.py
@@ -0,0 +1,6 @@
+letra = input("Informe a letra: ")
+
+if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+    print("A letra é uma vogal")
+else:
+    print("A letra é uma consoante")
